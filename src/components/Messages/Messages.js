4210810345984dc93a01ba8b027603724ce629d4
johannes60-sk <garilsenan@gmail.com>
2023-12-10 21:58:50 -0800

    feat: added message

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -10,14 +10,14 @@ const Messages = ({ messages, name }) => (
   
   <ScrollToBottom className="messages">
     {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
-    {/* <div  className="messagesStatic">Mon messsage</div>
     <div  className="messagesStatic">Mon messsage</div>
     <div  className="messagesStatic">Mon messsage</div>
     <div  className="messagesStatic">Mon messsage</div>
     <div  className="messagesStatic">Mon messsage</div>
     <div  className="messagesStatic">Mon messsage</div>
     <div  className="messagesStatic">Mon messsage</div>
-    <div  className="messagesStatic">Mon messsage</div> */}
+    <div  className="messagesStatic">Mon messsage</div>
+    <div  className="messagesStatic">Mon messsage</div>
   </ScrollToBottom>
 );
 
